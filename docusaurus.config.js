6d803fb1b5ecee15c925cecb12e2f6e9d370bdd7
Mika Kukkonen <mika.kukkonen@taikatilaus.fi>
2024-04-30 16:29:37 +0300

    Update editUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/TaikaTilaus/taikatilaus.github.io',
+            'https://github.com/TaikaTilaus/taikatilaus.github.io/blob/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/TaikaTilaus/taikatilaus.github.io',
+            'https://github.com/TaikaTilaus/taikatilaus.github.io/blob/main',
         },
         theme: {
           customCss: './src/css/custom.css',
