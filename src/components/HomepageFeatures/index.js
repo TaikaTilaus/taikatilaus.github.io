d5f1817a22ab7b3bc6de855e89756ccbe52ea7d6
Mika Kukkonen <mika.kukkonen@taikatilaus.fi>
2024-05-03 18:16:02 +0300

    Tekstimuutokset

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Pikaohjeita tavallisiin käyttötapauksiin.
       </>
     ),
     url: '/docs/pikaohjeet/asiakkaan-lisaaminen'
@@ -19,34 +18,52 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Yksityiskohtaiset ohjeet TaikaTilaus-sovelluksen käyttöön.
       </>
     ),
     url: '/docs/intro'
   },
   {
-    title: 'Uutiset',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'FAQ',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Usein kysyttyjä kysymyksiä TaikaTilaukseen liittyvistä aiheista.
       </>
     ),
-    url: '/blog'
+    url: '/docs/faq'
+  },  
+  {
+    title: 'Tukiportaalin ohjeet',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Freshdesk tukiportaalin ohjeet.
+      </>
+    ),
+    url: '/docs/pikaohjeet/asiakkaan-lisaaminen'
   },
   {
     title: 'Videot',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        TaikaTilaus-sovelluksen ohjevideoita.
       </>
     ),
-    url: '/videos'
+    url: 'docs/videos'
   },  
+  {
+    title: 'Uutiset',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Uutisarkisto ja versiotiedotteet.
+      </>
+    ),
+    url: '/blog'
+  },
+
 ];
 
 function Feature({Svg, title, description, url}) {
