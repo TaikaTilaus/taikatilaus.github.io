0f0f2dbc2a13b9bf4e64df9d4c92e55b76d1cb34
Siiri Lassila <siirilassila@gmail.com>
2024-07-03 15:41:04 +0300

    social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/taikatilaus-social-card.jpg',
+      image: 'img/social.jpg',
       navbar: {
         title: 'TaikaTilaus',
         logo: {
